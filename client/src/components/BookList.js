43905a32e413adb5bc67cb503bf5fa5883901a24
Saphal <gsaphal99@gmail.com>
2020-04-28 15:36:00 +0545

    Added Book

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,24 @@
 import React from "react";
-
+import { graphql } from "react-apollo";
+import { getBookQuery } from "../queries/queries";
 class BookList extends React.Component {
+  displayBooks = () => {
+    var data = this.props.data;
+    if (data.loading) {
+      return <div>Loading Books...</div>;
+    } else {
+      return data.books.map((book) => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  };
   render() {
     return (
       <div className="main">
-        <ul id="book-list">
-          <li>Book Name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
 }
 
-export default BookList;
+export default graphql(getBookQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -3,13 +3,15 @@ const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
 const app = express();
-
+const cors = require("cors");
 mongoose.connect(
   "mongodb+srv://saphal:123@cluster0-atyhd.mongodb.net/test?retryWrites=true&w=majority"
 );
 mongoose.connection.once("open", () => {
   console.log("Connected to Database");
 });
+app.use(cors());
+
 app.use(
   "/graphql",
   graphqlHTTP({
